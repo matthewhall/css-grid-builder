c684121f009f50b2494e886f95ff873738b40b18
Matt Hall <bizear@gmail.com>
2018-07-29 06:17:13 +0100

    Adds in additional about copy to AboutInfo component

--- src/components/AboutInfo/AboutInfo.js
+++ src/components/AboutInfo/AboutInfo.js
@@ -6,6 +6,7 @@ class AboutInfo extends Component {
       <div className="about">
         <h2>About</h2>
         <p>CSS Grid Layout is a method of using a grid concept to lay out content, providing a mechanism for authors to divide available space for layout into columns and rows using a set of predictable sizing behaviors.</p>
+        <p>You can read more on <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout">MDN</a>.</p>
       </div>
     );
   }
