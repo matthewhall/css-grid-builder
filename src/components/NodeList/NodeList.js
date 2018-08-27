6fb45056800dca71c72c6d1aa8e82d7e5fe667b1
Matt Hall <bizear@gmail.com>
2018-08-27 08:36:16 +0100

    Handles node clicks and passes inputted data to reducer

--- src/components/NodeList/NodeList.js
+++ src/components/NodeList/NodeList.js
@@ -3,36 +3,60 @@ import PropTypes from 'prop-types';
 
 import Node from '../Node';
 import Modal from '../Modal';
+import NodeSettings from '../NodeSettings';
 
 import './NodeList.scss';
 
 class NodeList extends Component {
+  static propTypes = {
+    lastNodeId: PropTypes.number,
+    nodes: PropTypes.array,
+    settings: PropTypes.object,
+    addNodeAtIndex: PropTypes.func,
+    removeNodeAtIndex: PropTypes.func
+  }
+
   constructor(props) {
     super(props);
 
     this.handleNodeClick = this.handleNodeClick.bind(this);
     this.handleDeleteNodeClick = this.handleDeleteNodeClick.bind(this);
+    this.handleNodeStyleChange = this.handleNodeStyleChange.bind(this);
 
     this.state = {
-      isModalOpen: false
+      isModalOpen: false,
+      editingNodeId: null,
+      editingNode: null
     };
   }
 
   handleAddNodeClick() {
     const index = this.props.nodes.length + 1;
+    const nextId = this.props.lastNodeId + 1;
 
     this.props.addNodeAtIndex({
       index: index - 1,
       node: {
+        className: `class-${nextId}`,
         text: 'div',
-        id: this.props.lastNodeId + 1
+        id: nextId,
+        styles: {}
       }
     });
   }
 
-  handleNodeClick() {
+  handleNodeClick(event) {
+    const id = Number(event.target.dataset.id);
+    const nodeData = this.props.nodes.find((node) => node.id === id);
+
+    if (!nodeData) {
+      return;
+    }
+
     this.setState({
-      isModalOpen: true
+      isModalOpen: true,
+      editingNodeId: id,
+      editingNode: nodeData
     });
   }
 
@@ -42,6 +66,18 @@ class NodeList extends Component {
     const index = this.props.nodes.findIndex((node) => node.id === id);
 
     this.props.removeNodeAtIndex(index);
+  }
+
+  handleNodeStyleChange(event) {
+    if (this.state.editingNodeId === null) {
+      return;
+    }
+
+    const id = this.state.editingNodeId;
+    const prop = event.target.name;
+    const value = event.target.value;
+
+    this.props.setNodeGridStyle(id, prop, value);
   }
 
   render() {
