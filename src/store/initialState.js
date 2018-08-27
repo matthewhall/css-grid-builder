4a146acdaadefbcc576cfd45d3e1977edd4aad8b
Matt Hall <bizear@gmail.com>
2018-08-27 08:34:30 +0100

    Sets up initial data for nodes

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -1,26 +1,28 @@
 export default {
   settings: {
-    'display': 'grid',
-    'gridColumnGap': '10px',
-    'gridRowGap': '10px',
-    'gridTemplateColumns': 'repeat(4, 1fr)',
-    'gridTemplateRows': '',
-    'gridTemplateAreas': '',
-    'justifyItems': '',
-    'alignItems': '',
-    'justifyContent': '',
-    'alignContent': ''
+    display: 'grid',
+    gridColumnGap: '10px',
+    gridRowGap: '10px',
+    gridTemplateColumns: 'repeat(4, 1fr)',
+    gridTemplateRows: '',
+    gridTemplateAreas: '',
+    justifyItems: '',
+    alignItems: '',
+    justifyContent: '',
+    alignContent: ''
   },
   nodes: {
     lastNodeId: 0,
     allNodes: [
       {
+        className: 'class-0',
         text: 'div',
-        id: 0
+        id: 0,
+        styles: {}
       }
     ]
   },
   sidePanel: {
     open: true
   }
-};
+}
