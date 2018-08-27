dbb36c6351bf7627a9173c2400b9045d2d87be70
Matt Hall <bizear@gmail.com>
2018-08-27 08:29:58 +0100

    Sets up Node component to accept styles

--- src/components/Node/Node.js
+++ src/components/Node/Node.js
@@ -3,23 +3,27 @@ import PropTypes from 'prop-types';
 
 class Node extends Component {
   static propTypes = {
+    id: PropTypes.number,
+    styles: PropTypes.objectOf(PropTypes.string),
     text: PropTypes.string,
     onClick: PropTypes.func,
     onDeleteButtonClick: PropTypes.func
   }
 
   static defaultProps = {
+    styles: {},
     text: 'div'
   }
 
   render() {
-    const { text, id, onClick, onDeleteButtonClick } = this.props;
+    const { text, styles, className, id, onClick, onDeleteButtonClick } = this.props;
 
     return (
-      <div className="node-list__node"
+      <div className={`node-list__node ${className}`}
           data-id={id}
           tabIndex="0"
-          onClick={onClick}>
+          style={styles}
+          onClick={($event) => onClick($event)}>
         <button className="node-list__node-delete-button"
             aria-label="Delete node"
             onClick={($event) => onDeleteButtonClick($event, id)}>+</button>
